f759d25a1e80a62f89f7ab9892da31a0b965e03d
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-07-05 21:18:42 -0300

    Create strings.js

---
+++ ProgramacionProcedural/strings.js
@@ -0,0 +1,33 @@
+//STRING DENTRO DE COMILLAS SIMPLES
+  var texto = 'Este es mi string.'
+
+//STRING DENTRO DE COMILLAS DOBLES
+  var texto = "Este es mi string."
+
+//STRING DENTRO DE ACENTOS GRAVES
+  var texto = `Este es mi string.`
+
+
+//USO DE COMILLAS DOBLES DENTRO DE COMILLAS SIMPLES
+  var texto = 'Este es mi "string".'
+  
+//USO DE ACENTOS GRAVES DENTRO DE COMILLAS SIMPLES
+  var texto = 'Este es mi `string`.'
+
+//USO DE COMILLAS SIMPLES DENTRO DE COMILLAS DOBLES
+  var texto = 'Este es mi "string".'
+  
+//USO DE ACENTOS GRAVES DENTRO DE COMILLAS DOBLES
+  var texto = 'Este es mi `string`.'
+  
+  
+//USO DE COMILLAS SIMPLES DENTRO DE COMILLAS SIMPLES
+  var texto = 'Este es mi \'string\'.'
+  
+//USO DE COMILLAS DOBLES DENTRO DE COMILLAS DOBLES
+  var texto = "Este es mi \"string\"."
+  
+//USO DE ACENTOS GRAVES DENTRO DE ACENTOS GRAVES
+  var texto = `Este es mi \`string\`.`
+  
+  
