10e871a150d7b8a0556db5810571cfa757416689
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-10-13 05:16:29 -0300

    Create predefinidos.js

---
+++ Funciones/ElementosHTML/predefinidos.js
@@ -0,0 +1,13 @@
+//Siguiente elemento:
+  e.nextElementSibling
+	e.nextSibling
+//Anterior elemento:
+	e.previusElementSibling
+	e.previusSibling
+//Elemento padre:
+	e.parentElement
+	e.parentNode
+//Eliminar elemento:
+	e.remove()
+//Modificar elemento completo:
+	e.outerHTML="..."
